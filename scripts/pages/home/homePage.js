345dbfaf70b3e4d49c02ca3e50ccde9f2cad380a
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-01-09 12:21:45 +0100

    udate

--- scripts/pages/home/homePage.js
+++ scripts/pages/home/homePage.js
@@ -1,4 +1,4 @@
-//import Filter from "./filterTags.js";
+import Filter from "./filterTags.js";
 import header from "../header/header.js";
 
 // AFFICHAGE DE TOUS LES PHOTOGRAPHES PAR DEFAUT
@@ -24,15 +24,15 @@ export default class homePage{
             <p class="location"> ${photographe.city}, ${photographe.country} </p>
             <p class="tagline">${photographe.tagline}</p>
             <p class="price"> ${photographe.price}</p>
-            <ul class = "filter">${photographe.tags.map(tag => {
+            <ul class = "filter">${photographe.tags.map(tag => 
                 `<li data-filter = "${tag}"> #${tag}</li>`
-            }).join(" ")}
+            ).join(" ")}
             </ul>
                         `
             photographerSections.appendChild(photographerArticles);
             photographerArticles.innerHTML = templatePhotographer;
         });
-        //new Filter().filterTags();
+        new Filter().filterTags();
 
         new header().entete();
     }
