31fb5341a6528c6c9478455f52ccf1bf1bed5c72
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-02-02 19:37:03 +0100

    new update

--- scripts/pages/photographers/LightBox.js
+++ scripts/pages/photographers/LightBox.js
@@ -6,7 +6,10 @@ export default class LightBox {
 
     // initialisation de la lightbox
     initLightBox(currentMedia, currentMediaName) {
+        let gauche = document.querySelector('.left-arrow-lightbox');
+        let droit = document.querySelector('.right-arrow-lightbox');
         let getMedias = Array.from(document.getElementsByClassName('ph-media'));
+
         getMedias.forEach((mediaWorks, index) => mediaWorks.addEventListener('click', () => {
             let lightBoxMedia = document.getElementById('works-lightbox-media');
             let lightBoxName = document.getElementById('works-lightbox-name');
@@ -18,13 +21,19 @@ export default class LightBox {
             lightBoxMedia.innerHTML = `${src}`;
             lightBoxName.innerHTML = `${nameSrc}`;
         }))
-        this.previous(document.querySelector('.left-arrow-lightbox'), currentMedia, currentMediaName);
-        this.next(document.querySelector('.right-arrow-lightbox'), currentMedia, currentMediaName);
+        this.previous(gauche, currentMedia, currentMediaName);
+        this.next(droit, currentMedia, currentMediaName);
         this.close();
         this.keyboard(currentMedia, currentMediaName);
         return this;
     }
 
     //fonction de retour au media  precedent 'previous'
-    
+    previous(elt, media, name) {
+        elt.addEventListener('click', () => {
+            this.currentIndex -= 1;
+            let lightBoxMedia = document.getElementById('works-lightbox-media');
+            let lightBoxName = document.getElementById('works-lightbox-name');
+        })
+    }
 }
