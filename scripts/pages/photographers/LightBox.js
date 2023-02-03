d538c2299c8fbaf4dc8154cfa23348dac76b088b
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-02-03 18:56:07 +0100

    NEW UPDATE LIGTHBOX

--- scripts/pages/photographers/LightBox.js
+++ scripts/pages/photographers/LightBox.js
@@ -28,12 +28,107 @@ export default class LightBox {
         return this;
     }
 
-    //fonction de retour au media  precedent 'previous'
+    // 4) fonction de retour au media  precedent 'previous'
     previous(elt, media, name) {
+
         elt.addEventListener('click', () => {
             this.currentIndex -= 1;
+
             let lightBoxMedia = document.getElementById('works-lightbox-media');
             let lightBoxName = document.getElementById('works-lightbox-name');
+
+            if (this.currentIndex < 0) {
+                this.currentIndex = media.length - 1;
+                this.currentIndex = name.llenth - 1;
+            }
+
+            let src = media[this.currentIndex];
+            let nameSrc = name[this.currentIndex];
+
+            lightBoxMedia.innerHTML = `${src}`;
+            lightBoxName.innerHTML = `${nameSrc}`;
         })
     }
+
+     // 5) fonction de d'aller au media  suivant 'previous'
+
+     next(elt, media, name) {
+
+        elt.addEventListener('click', () => {
+
+            this.currentIndex += 1;
+            
+            let lightBoxMedia = document.getElementById('works-lightbox-media');
+            let lightBoxName = document.getElementById('works-lightbox-name');
+
+            if (this.currentIndex > name.length - 1) {
+                this.currentIndex = 0;
+            }
+            
+            let src = media[this.currentIndex];
+            let nameSrc = name[this.currentIndex];
+
+            lightBoxMedia.innerHTML = `${src}`;
+            lightBoxName.innerHTML = `${nameSrc}`;
+        });
+     }
+
+     // 6) fer;eture de lq light box
+
+     close() {
+        let closeIcon = document.querySelector('.close-lightbox-icon');
+        closeIcon.addEventListener('click', () => {
+            let lightbox = document.getElementById('works-lightbox');
+
+            lightbox.style.display = 'none';
+        })
+     }
+
+     keyboard(currentMedia, currentMediaName) {
+        document.addEventListener('keydown', (key) => {
+
+            console.log('key', key);
+
+            let lightBoxMedia = document.getElementById('works-lightbox-media');
+            let lightBoxName = document.getElementById('works-lightbox-name');
+
+            //PRESSER LA TOUCHE "Esc" pour fermer la light box
+
+            if (key.code == "Escape") {
+                let lightbox = document.getElementById('works-lightbox');
+                lightbox.style.display = 'none';
+            }
+
+            // click a droit pour acceder a l'element de droit
+            else if (key.code == "ArrowRight") {
+                this.currentIndex += 1;
+
+                if (this.currentIndex > currentMediaName.length - 1) {
+                    this.currentIndex = 0;
+                }
+
+                let src = currentMedia[this.currentIndex];
+                let nameSrc = currentMediaName[this.currentIndex];
+
+                lightBoxMedia.innerHTML = `${src}`;
+                lightBoxName.innerHTML = `${nameSrc}`;
+            }
+
+            // click a guauche pour acceder a l'elt de gauche
+            else if (key.code == "ArrowLeft") {
+                this.currentIndex -= 1;
+
+                if (this.currentIndex < 0) {
+                    this.currentIndex = currentMedia.length - 1;
+                    this.currentIndex = currentMediaName.length - 1;
+                }
+
+                let src = currentMedia[this.currentIndex];
+                let nameSrc = currentMediaName[this.currentIndex];
+
+                lightBoxMedia.innerHTML = `${src}`;
+                lightBoxName.innerHTML = `${nameSrc}`;
+            }
+        });
+     }
 }
